eaf63e2205653fb286e18e96d430198c7cbe4cde
Prathamesh Bhoge <Prathameshbhoge.cool@gmail.com>
2024-03-19 01:34:15 +0530

    initial commit

--- src/main/java/com/Yt/LinkedList/ConvertArrayToLinkedList.java
+++ src/main/java/com/Yt/LinkedList/ConvertArrayToLinkedList.java
@@ -1,2 +1,28 @@
-package main.java.com.Yt.LinkedList;public class ConvertArrayToLinkedList {
+package main.java.com.Yt.LinkedList;
+
+public class ConvertArrayToLinkedList {
+    public static void main(String[] args) {
+       int arr[]={10,20,30,40,50,60,70};
+        Node head = Converter(arr);
+        Display(head);
+    }
+
+    static Node Converter(int arr[]){
+        Node head= new Node(arr[0]);
+        Node move=head;
+        for(int i=1;i< arr.length;i++){
+            Node temp = new Node(arr[i]);
+            move.next=temp;
+            move= move.next;
+        }
+        return head;
+    }
+    static void Display(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data+" -> ");
+            curr= curr.next;
+        }
+        System.out.println();
+    }
 }
--- src/main/java/com/Yt/LinkedList/CreateAList.java
+++ src/main/java/com/Yt/LinkedList/CreateAList.java
@@ -1,2 +1,10 @@
-package main.java.com.Yt.LinkedList;public class CreateAList {
+package main.java.com.Yt.LinkedList;
+
+public class CreateAList {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        Node node = new Node(30);
+        System.out.println(node.data);
+    }
+
 }
